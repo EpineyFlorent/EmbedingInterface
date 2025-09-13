dff3622a2f46ed0ceb04eaa2501e0086e35fcea7
FlorentEpiney <florent.epiney@netplus.ch>
2025-09-13 20:33:51 +0200

    feat: Include exe with pythons in one build

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const ElectronStore = require('electron-store')
 const path = require('path')
 const { spawn } = require('child_process')
+const isDev = !app.isPackaged;
+const pythonExe = isDev
+    ? path.join(__dirname, 'dist', 'app.exe')
+    : path.join(process.resourcesPath, 'app.exe');
+const { execFile } = require('child_process');
+
 
 // Initialisation du store
 const store = new ElectronStore({
@@ -43,7 +49,10 @@ function createWindow() {
 
   ipcMain.handle('index-documents', async (event, config) => {
     return new Promise((resolve, reject) => {
-        const pythonProcess = spawn('python', ['app.py', JSON.stringify(config)])
+        const pythonProcess = spawn(pythonExe, [JSON.stringify({
+            command: 'index',
+            ...config
+        })]);
         let output = ''
         let error = ''
 
@@ -68,9 +77,15 @@ function createWindow() {
 
 ipcMain.handle('query', async (event, query) => {
 return new Promise((resolve, reject) => {
-  const process = spawn('python', ['app.py', JSON.stringify({ command: 'query', query: query })])
-  let output = ''
-  let error = ''
+    const process = spawn(pythonExe, [JSON.stringify({
+        command: 'query',
+        data_dir: store.get('appConfig.data_dir'),
+        embeddings_file: store.get('appConfig.embeddings_file'),
+        model: store.get('appConfig.model'),
+        query: query
+    })]);
+    let output = ''
+    let error = ''
 
   process.stdout.on('data', (data) => {
     output += data.toString()
